cde6f765ea1ab260d9732e4c57ecdccd2a526595
Abhishek-Khemnar <khemnarabhishek3@gmail.com>
2025-09-29 11:33:09 +0530

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//interger division problem
+int main()
+{
+    int total;
+    int obtained ;
+    float percentage;
+
+    total = 500;
+    obtained = 473;
+
+    percentage = (obtained/total)*100;
+
+    printf("prcentage obtained is %f",percentage);
+
+    return 0;
+}
